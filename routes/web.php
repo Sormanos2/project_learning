8d6e39458b0254dee6350573c2abc8babe8cbef1
Tob Sormanos <sormanos.tob@prasac.com.kh>
2020-07-02 13:25:48 +0700

    second commit

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('/admin','AdminsController@index')->name('admin.index');
+Route::get('/post','PostController@show')->name('post'); 
