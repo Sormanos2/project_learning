d5d930312216d31e02bbac45734f64ecb7574ced
Tob Sormanos <sormanos.tob@prasac.com.kh>
2020-07-08 18:09:56 +0700

    first blog

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
-    public function show(){
-        return view('blog-post');
+    public function show($id){
+
+        $post=Post::findorFail($id);
+
+        return view('blog-post', compact('post'));
     }
 }
